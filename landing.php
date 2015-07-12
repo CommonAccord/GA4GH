5258a66fb6168916dca4ece51bd6962548c8c6db
James Hazard <james.g.hazard@gmail.com>
2015-07-12 19:14:43 +0200

    better?

--- landing.php
+++ landing.php
@@ -3,7 +3,7 @@
 <table><tr><td>Welcome to a subdomain of CommonAccord.
 
 
-	<br> Here: <a href="index.php?action=list&file=/Demo/">The Patient Consent Demo</a> showing how to make machine readable the model consent form of the REWG of the Global Alliance for Genomics and Health. In English, French and German.<br><br>
+	<br> Medical research could advance more quickly if it were possible to make the "consent" process fully clear for participants and machine readable for researchers.<br><br>Treating the text as a wiki can accelerate both goals. <br><br>Here, as an example, is a <a href="index.php?action=list&file=/Demo/">The Patient Consent Demo</a> based on the REWG model consent form of the Global Alliance for Genomics and Health. In English, French and German.<br><br>
 <br> Here: <a href="index.php?action=list&file=/">Documents</a>:<br><br>
 	See also <ul><li><a href="http://www.commonaccord.org">commonaccord.org</a><li><a href="http://www.fr.commonaccord.org">fr.commonaccord.org</a><li><a href="http://www.ga4gh.commonaccord.org">ga4gh.commonaccord.org</a></ol>.<br>
 
